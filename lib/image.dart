1e40f3f96ce3a93edc552100e99837c5fa0675a4
Muhammad Ali Nizami <kustalics@gmail.com>
2024-09-09 11:39:20 +0500

    Image Done

---
+++ lib/image.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ImageViewDemo extends StatelessWidget {
+  const ImageViewDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Image Demo"),
+      ),
+      body: Column(
+        children: [
+          Image.network(
+            "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Image.asset('assets/images/1.png')
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:usama_project/image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,67 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          centerTitle: true,
-          leading: Icon(
-            Icons.arrow_back_ios,
-            color: Colors.white,
-            size: 18,
-          ),
-          title: Text(
-            "First Class",
-            style: TextStyle(
-                color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
-          ),
-          actions: [
-            Icon(
-              Icons.notifications,
-              color: Colors.white,
-            ),
-            Icon(
-              Icons.shopping_cart,
-              color: Colors.white,
-            ),
-          ],
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "This ",
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              width: 30,
-            ),
-            Text(
-              "This is second text This is second text",
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 40,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-              Icon(Icons.thumb_up),
-              Icon(Icons.comment),
-              Icon(Icons.comment),
-
-              Icon(Icons.share),
-            ],)
-          ],
-        ),
-        // backgroundColor: Colors.blue,
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.yellow,
-          onPressed: () {},
-          child: Icon(
-            Icons.add,
-            color: Colors.blue,
-          ),
-        ),
-      ),
+      home: ImageViewDemo(),
     );
   }
 }
