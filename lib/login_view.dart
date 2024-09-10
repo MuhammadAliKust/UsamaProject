098792bb6a354b9b75bab8bca1c483d511b37619
Muhammad Ali Nizami <kustalics@gmail.com>
2024-09-10 11:16:25 +0500

    Login Done

---
+++ lib/login_view.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  LoginView({super.key});
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController pwdController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Column(
+        children: [
+          TextFormField(
+            controller: emailController,
+            decoration: InputDecoration(
+              label: Text("Email"),
+              hintText: 'test@gmail.com',
+              prefixIcon: Icon(Icons.email),
+              suffixIcon: Icon(Icons.email),
+            ),
+          ),
+          TextFormField(
+            controller: pwdController,
+            decoration: InputDecoration(
+              label: Text("Password"),
+              hintText: 'Password must be 6 or more characters.',
+              prefixIcon: Icon(Icons.lock),
+              suffixIcon: Icon(Icons.visibility),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                if (emailController.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Email cannot be empty.")));
+                  return;
+                }
+                if (pwdController.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Password cannot be empty.")));
+                  return;
+                }
+                if (pwdController.text.length < 6) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Password must be 6 or more characters.")));
+                  return;
+                }
+              },
+              child: Text("Login"))
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:usama_project/image.dart';
+import 'package:usama_project/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ImageViewDemo(),
+      home: LoginView(),
     );
   }
 }
