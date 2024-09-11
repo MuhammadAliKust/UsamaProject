ecbbc5dbc278433da623bd5ba808cf4059a467c4
Muhammad Ali Nizami <kustalics@gmail.com>
2024-09-11 11:41:56 +0500

    Image Dimension Topic Covered

---
+++ lib/list_view_builder.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ListViewDemo extends StatefulWidget {
+   ListViewDemo({super.key});
+
+  @override
+  State<ListViewDemo> createState() => _ListViewDemoState();
+}
+
+class _ListViewDemoState extends State<ListViewDemo> {
+  int selectedIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List View Demo"),
+      ),
+      body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, i) {
+            return ListTile(
+              onTap: (){
+                // selectedIndex = i;
+                // setState(() {
+                //
+                // });
+              },
+              leading: Icon(Icons.notifications),
+              title: Text("Notification Title $selectedIndex"),
+              subtitle: Text("Notification Sub Title"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              tileColor: Colors.blue,
+            );
+          }),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:usama_project/image.dart';
-import 'package:usama_project/login_view.dart';
+import 'package:usama_project/list_view_builder.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ImageViewDemo(),
+      home: ListViewDemo(),
     );
   }
 }
